fc8e1a0118049d306178a01e1afb34239c313e27
Jusabe Guedes <jusabe.09@gmail.com>
2019-01-21 11:06:02 +0100

    update deps

--- mix.exs
+++ mix.exs
@@ -2,16 +2,18 @@ defmodule JaSerializer.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :ja_serializer,
-     version: "0.12.0",
-     elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     consolidate_protocols: Mix.env != :test,
-     source_url: "https://github.com/vt-elixir/ja_serializer",
-     package: package(),
-     description: description(),
-     deps: deps()]
+    [
+      app: :ja_serializer,
+      version: "0.12.0",
+      elixir: "~> 1.1",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      consolidate_protocols: Mix.env() != :test,
+      source_url: "https://github.com/vt-elixir/ja_serializer",
+      package: package(),
+      description: description(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -24,14 +26,14 @@ defmodule JaSerializer.Mixfile do
       {:inflex, "~> 1.4"},
       {:plug, "> 1.0.0"},
       {:poison, ">= 1.4.0"},
-      {:ecto, "~> 1.1 or ~> 2.0", only: :test},
+      {:ecto, "~> 2.0 or ~> 3.0", only: :test},
       {:earmark, "~> 0.1", only: :dev},
       {:inch_ex, "~> 0.4", only: :docs},
       {:scrivener, "~> 1.2 or ~> 2.0", optional: true},
       {:benchfella, "~> 0.3.0", only: :dev},
       {:ex_doc, "~> 0.7", only: :dev},
       {:dialyxir, "~> 0.3.5", only: :dev},
-      {:credo, "~> 0.4.11", only: :dev},
+      {:credo, "~> 0.4.11", only: :dev}
     ]
   end
 
@@ -41,7 +43,7 @@ defmodule JaSerializer.Mixfile do
       maintainers: ["Alan Peabody"],
       links: %{
         "GitHub" => "https://github.com/vt-elixir/ja_serializer"
-      },
+      }
     ]
   end
 
